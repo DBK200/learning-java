3051df4b7e27f30c5a6c2bf5d8fe1661914a6054
ConstantinMihai4356 <mihai_4story921@yahoo.com>
2023-07-04 19:40:53 +0300

    Update

--- src/main/java/com/sessions/MyApplication.java
+++ src/main/java/com/sessions/MyApplication.java
@@ -5,8 +5,7 @@ public class MyApplication {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
-        System.out.println("some updates");
-        System.out.println("some more updates");
+
         
     }
 
