1378e5315ae7f40772713b14657f3b97bc55df6b
teo332 <bulzanteodor330@yahoo.com>
2023-07-04 20:23:00 +0300

    .

--- src/main/java/com/sessions/MyApplication.java
+++ src/main/java/com/sessions/MyApplication.java
@@ -4,10 +4,11 @@ public class MyApplication {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+
         System.out.println("some updates");
         System.out.println("some more updates");
         System.out.println("even more updates");
+
         
     }
 
