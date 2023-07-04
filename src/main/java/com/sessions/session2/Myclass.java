537ea93114103df5bf97f613f0285216f14e4cd1
catalin-martazan <catalin.martazan@gmail.com>
2023-07-04 20:03:18 +0300

    Second entry

--- src/main/java/com/sessions/session2/Myclass.java
+++ src/main/java/com/sessions/session2/Myclass.java
@@ -2,6 +2,6 @@ package com.sessions.session2;
 
 public class Myclass {
     public static void main(String[] args) {
-        System.out.println("something else...");
+        System.out.println("something else new...");
     }
 }
