17205898bcd975d1684f9cb28fa6af2934e967a3
Roxanaa1 <roxanaenache2003@gmail.com>
2023-07-14 21:02:09 +0300

    Tema

--- src/main/java/com/sessions/session3/OperatorsPractice.java
+++ src/main/java/com/sessions/session3/OperatorsPractice.java
@@ -1,113 +1,143 @@
 package com.sessions.session3;
 
-public class OperatorsPractice {
+public class OperatorsPractice
+{
+    public static void main(String[] args)
+    {
+//post-unary operators expression++, expression--
 
-    public static void main(String[] args) {
+        int x=0;
+        int y=2;
+        int z=y++;
+        int r=x--;
+        System.out.println(x++);
+        System.out.println(y++);
+        System.out.println(r);
+        System.out.println("x= " +y);
 
-        //post-unary operators expression++, expression--
-//        int x = 0;
-//        int y = x++;
-//        System.out.println("x = " + x);
-//        System.out.println("y = " +y);
 
         //pre-unary operators ++expression, --expression
-//        int x = 0;
-//        int y = ++x;
-//        System.out.println("x = " + x);
-//        System.out.println("y = " +y);
-//
-//        x += 2;
-//        System.out.println("x = " + x);
+
+        int f=0;
+        f+=4;
+        System.out.println("f = " + x);
+
 
         //other unary operators +,-,!
-//        int x = 1;
-//        int y = -x;
-//        System.out.println("x = " + x);
-//        System.out.println("y = " + y);
-//
-//        boolean z = true;
-//        boolean w = !z;
-//        System.out.println("x = " + z);
-//        System.out.println("y = " + w);
+        int j = -x;
+        int V=j;
+        System.out.println("x = " + x);
+        System.out.println("V=");
+        System.out.println(V);
+
 
         //Multiplication/Division/Modulus *, /, %
-//        int x = 3;
-//        int y = 2;
-//        System.out.println(x * y); // order of operations
-//        System.out.println( x % y);
+
+        int s = 4;
+        int w = 9;
+        System.out.print("s*w=");
+        System.out.println(s*w);
+        System.out.print("s%2=");
+        System.out.println( s % 2);
+        System.out.print("s/w=");
+        System.out.println(s/w);
+
 
         //Addition/Subtraction +, -
-//        int x = 2 + 3 - 1;
-//        System.out.println(x);
-//
-//        int x = 2 * 5 + 3 * 4 - 8;
-//        System.out.println(x);
-//
-//        x = 2 * (5 + 3) * (4 - 8);
-//        System.out.println(x);
-//
-//        x = 2 * (5 + 3 * (4 - 8));
-//        System.out.println(x);
+
+        int t=4*3+8/2-5*3;
+        int l=3-8+4/8*4;
+        System.out.print("t=");
+        System.out.println(t);
+        System.out.print("l=");
+        System.out.println(l);
+
 
         //NUMERIC PROMOTIONS
         // 1 .If two values have different data types, Java will automatically promote one of the values
         //to the larger of the two data types.
-
 //        int x = 2; // range from –2,147,483,648 to 2,147,483,647. (4bytes)
 //        long y = 3; // range –9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 (8bytes)
-
 //        int sum = x + y; //does not compile
-//        long sum = x + y;
-//        System.out.println(sum);
+
+        double p=3;
+        long k=3;
+        double sum=p+k;
+        double produs=p*k;
+        System.out.print("suma=");
+        System.out.println(sum);
+        System.out.print("produs=");
+        System.out.println(produs);
+
 
 
         // 2. If one of the values is integral and the other is floating-point, Java will automatically
         //promote the integral value to the floating-point value’s data type.
-//        int x = 2;
-//        double y = 3.2;
-//        double sum = x+y;
-//        System.out.println(sum);
+        int u = 1;
+        double q = 6.2;
+        double suma = u+q;
+        System.out.print("suma=");
+        System.out.println(suma);
+
 
         //3. Smaller data types, byte, short, and char, are first promoted to int any time
         //they’re used with a Java binary arithmetic operator, even if neither of the operands is
         //int.
-//        short x = 1;
-//        short y = 2;
-//        short sum = x + y; //does not compile
-//        int sum = x + y;
-//        System.out.println(sum);
+
+        short m = 3;
+        short b = 2;
+        int c=3;
+        double e=6;
+        //short scadere = m - b; //does not compile
+        int scadere = m - b ;
+        double sumaaa=c+e;
+        System.out.print("Scadere=");
+        System.out.println(scadere);
+        System.out.print("Suma=");
+        System.out.println(sumaaa);
+
 
         //4. After all promotion has occurred and the operands have the same data type, the resulting
         //value will have the same data type as its promoted operands.
-//        float result1 = 2 + 3L + 2.3f;
-//        double result2 = 2 + 3L + 2.3f + 3.4;
+        float result1 = 2 + 3L + 2.3f;
+        double result2 = 2 + 3L + 2.3f + 3.4;
+        long result3=2+5*6+4L+3;
+        int result4=3+7-8/3-8*4;
+        short result5=1+2_1+1*1-1/1;
+
+
 
 // ----------------------------------------------------------------------------
         //CASTING PRIMITIVE VALUES -
         // convert from floating-point to integral value
-//        int x = 1.0; // does not compile
-//        int x = (int) 1.0;
+        //int X = 1.0; // does not compile
+        int X = (int) 1.0;
         //going from a larger numerical data type to a smaller numerical data type
-//        int x = (int) 1l;
-//        System.out.println(x);
+        int G = (int) 1l;
+        System.out.print("G=");
+        System.out.println(G);
 
         //Overflow
-//        long x = 40000;
-//        System.out.println("Long value is " + x);
-//        short y = (short) x; // short minimum value of -32,768 and a maximum value of 32,767
-//        System.out.println("Short value is " + y);
+        long Y = 40000;
+        System.out.println("Long value is " + x);
+        short I = (short) x; // short minimum value of -32,768 and a maximum value of 32,767
+        System.out.println("Short value is " + y);
 
 //        System.out.println(2147483647+1); // 2147483647 is the maximum int value, -2147483648
 //        System.out.println(2147483647+2);
 //        System.out.println(10^(-100)); // 2147483647 is the maximum int value, -2147483648
 
+
         //Underflow
-//        double x = Math.pow(2, -1074);
-//        System.out.println(x);
-//        x = Math.pow(2, -1075);
-//        System.out.println(x);
-//        x = Math.pow(2, -1076);
-//        System.out.println(x);
+        double J = Math.pow(2, -1074);
+        System.out.print("J=");
+        System.out.println(J);
+        double  N = Math.pow(2, -1075);
+        System.out.print("N=");
+        System.out.println(N);
+        double  L = Math.pow(2, -1076);
+        System.out.print("L=");
+        System.out.println(L);
 
 
 
@@ -115,37 +145,53 @@ public class OperatorsPractice {
         //Relational operators <, >, <=, >=, instanceof
         //Equal to/not equal to ==, !=
         //Logical operators &, ^, |
-//        boolean x = true | false;
-//        System.out.println(x);
-//
-//        boolean y = true & true;
-//        System.out.println(y);
-//
-//        boolean z = true ^ false;
-//        System.out.println(z);
+        boolean B = true | false;
+        boolean M=true & false;
+        boolean W = true ^ true;
+        System.out.print("B=");
+        System.out.println(B);
+        System.out.print("M=");
+        System.out.println(M);
+        System.out.print("W=");
+        System.out.println(W);
+
 
         //Short-circuit logical operators &&, ||
-//        int x = 0;
-////        boolean y = true | 1<++x;
-//        boolean y = true || 1<++x;
-//        System.out.println(y);
-//        System.out.println(x);
+        int d = 0;
+        boolean D = true | 1<++x;
+        boolean A = true || 1<++x;
+        System.out.print("D=");
+        System.out.println(D);
+        System.out.print("A=");
+        System.out.println(A);
 
         //Ternary operators boolean expression ? expression1 : expression2
-//        String x = 3 < 2 ? "rosu" : "albastru";
-//        System.out.println(x);
+        String a = 5 < 2 ? "alb" : "albastru";
+        System.out.print("a=");
+        System.out.println(a);
 
         //Assignment operators =, +=, -=, *=, /=, %=, &=, ^=, !=, <<=, >>=, >>>=
-//        int x = 1;
-//        System.out.println(x);
-//        x += 2; // x=x+2;
-//        System.out.println(x);
+
+        int Z = 1;
+        System.out.print("Z=");
+        System.out.println(Z);
+        Z += 2; // Z=Z+2;
+        System.out.print("Z=");
+        System.out.println(Z);
+
 
         //Shift operators <<, >>, >>>
-//        int x=1; //1 is 0001 in binary,
-//        System.out.println(x << 1); // shift 1 bit to left means 0010 which is 2
-//        System.out.println(x << 2); // shift 2 bits to left means 0100 which is 4
+
+        int H=1; //1 is 0001 in binary,
+        int T=3;
+        System.out.println(H << 1); // shift 1 bit to left means 0010 which is 2
+        System.out.println(H << 2); // shift 2 bits to left means 0100 which is 4
+        System.out.print("T<<1=");
+        System.out.println(T<<1);
+        System.out.print("T<<2=");
+        System.out.println(T<<2);
+        System.out.println("   ");
+
 
     }
-
 }
--- src/main/java/com/sessions/session4/Statements.java
+++ src/main/java/com/sessions/session4/Statements.java
@@ -1,203 +1,197 @@
 package com.sessions.session4;
 
-public class Statements {
-
-
-    public static void main(String[] args) {
-
+public class Statements
+{
+    public static void main(String[] args)
+    {
         //conditional statements
         //if-then
-//        int x =1;
-//        if(x<2){
-//            System.out.println("x is smaller than 2");
-//        }
-//        checkNumber(x);
+
+        int x = 3;
+        if (x > 1) {
+            System.out.println("X este mai mare ca 1");
+        }
+
 
         //ternary operator - condensed form of an if else that returns a value
-//        int x = 2 < 3 ? 4 : 5;
+        String y = 4 > 1 ? "da" : "nu";
+
 
         //if else
-//        String car = "BMWn";
-//         if (car.equals("BMW")){
-//             System.out.println("Car is a bmw");
-//         }else {
-//             System.out.println("Car is not a bmw");
-//         }
+//
+        int d = 3;
+        if (d % 2 == 0) {
+            System.out.println("d este par");
+        } else {
+            System.out.println("d este impar");
+        }
+
 
         //if else if
-//        String dayOfWeek = "Monday";
-//        if(dayOfWeek.equals("Wednesday")){
-//            System.out.println("Discount is 10%");
-//        }else if(dayOfWeek.equals("Thursday")){
-//            System.out.println("Discount is 15%");
-//        }else if(dayOfWeek.equals("Monday")){
-//            System.out.println("Discount is 20%");
-//        }else{
-//            System.out.println("no discount today");
-//        }
+
+        int e = -2;
+        if (e > 0) {
+            System.out.println("Numarul este pozitiv");
+        } else if (e < 0) {
+            System.out.println("Numarul este negativ");
+        } else {
+            System.out.println("Numarul este neutru");
+        }
+
 
         //switch
-//        String dayofWeek = "Monday";
-//        switch (dayofWeek) {
-//            case "Wednesday":
-//                System.out.println("Discount is 10%");
-//                break;
-//            case "Monday":
-//                System.out.println("Discount is 20%");
-//                break;
-//            case "Thursday":
-//                System.out.println("Discount is 15%");
-//                break;
-//            default:
-//                System.out.println("no discount today");
-//
-//        }
+
+        String LunileAnului = "MartieAprilieMai";
+        switch (LunileAnului) {
+            case "IunieIulieAugust":
+                System.out.println("Este anotimpul vara");
+                break;
+            case "MartieAprilieMai":
+                System.out.println("Este anotimpul primavara");
+                break;
+            case "SeptembrieOctombrieNoiembrie":
+                System.out.println("Este anotimpul toamna");
+                break;
+            case "DecembrieIanuarieFebruarie":
+                System.out.println("Este anotimpul iarna");
+                break;
+            default:
+                System.out.println("Ati introdus gresit!");
+        }
+
 
         //Loop statements
 
         //arrays
-//        int[] numbers = {1, 2, 3, 4, 5};
-//        int[][] numberss = {{1, 2, 3}, {4, 5, 6},{9}};
-//        System.out.println(numberss[2][0]);
-//
-//        String[] daysOfWeek = {"Monday", "Tuesday"};
-//        String days[] = {};
-//
-//        int[] numerz= new int[10];
-//        System.out.println(numerz.length);
-//        numerz[0]=2;
-//        System.out.println(numerz[0]);
-//
+
+        String[] luna = {"F", "E", "B", "R", "U", "A", "R", "I", "E"};
+        System.out.println(luna[3]);
+
+        int[] X = {2, 4, 4, 1, 6};
+        System.out.println(X[0]);
+
+
 //        int[] noNumber={};
 //        noNumber[0]=1; < index out of bounds exception
 
         //for loop
 
-//        int[] numbers = {1, 2, 3, 4, 5, 6, 3, 4};
-//        System.out.println(numbers[0]);
-//        System.out.println(numbers[1]);
-//
-//        for (int i = 0; i < numbers.length; i++) {
-////        for (int i = 0; i <= numbers.length; i++) {
-//            System.out.println(numbers[i]);
-//        }
+        String[][] Luna = {{"A", "U", "G"},
+                {"U", "S", "T"}};
+        for (int i = 0; i < Luna.length; i++) {
+            for (int j = 0; j < Luna[i].length; j++) {
+                System.out.println(Luna[i][j]);
+            }
+        }
 
         //for each
-//        for (int number: numbers) {
-//            System.out.println(number);
-//        }
+
+        int[] numere = {2, 5, 3, 4, 7, 8, 5, 2};
+        for (int numar : numere) {
+            System.out.print(numar);
+        }
 //
-//        String[] myMusicPreferences = {"House", "Jazz", "Dance"};
-//
-//        for (String music: myMusicPreferences ) {
-//            System.out.println(music);
-//        }
 
         //while
-//        int x = 0;
 
-//        while(x<10){
-//            System.out.println(x);
-//            x++;
-//        }
-//
-//        while (x < 0) {
-//            System.out.println(x);
-//            x++;
-//        }
+        int c = 2;
+        while (c < 10) {
+            System.out.println(c);
+            c = c + 1;
+        }
+        System.out.println(" ");
+        System.out.println(" ");
+
 
         //do-while
+        int p = 3;
+        do {
+            System.out.println(d);
+            d++;
+        } while (d < 9);
 
-//        do {
-//            System.out.println(x);
-//            x++;
-//        } while (x < 10);
-//
-//        do {
-//            System.out.println(x);
-//            x++;
-//        } while (x < 0);
 
         //flow control
 
-        //return
-//        int x = 3;
-//        if (x > 2) {
-//            return; // exits the execution of this method
-//        }
-//        System.out.println("something");
-//        System.out.println("something");
-//        System.out.println("something");
+        //return-exits the execution of this method
+
+       /*
+       `int b=1;
+        if(b<4)
+        {
+            return;
+        }
+        System.out.println("salut");
+        */
+
 
         //break - used to exit a loop or a switch statement
 
-//        int[] numbers = {1, 2, 3, 4, 5};
-//
-//        for (int i = 0; i < numbers.length; i++) {
-//            System.out.println(numbers[i]);
-//            if (numbers[i] == 3) break;
-//            System.out.println(numbers[i] + " is lower than 3");
-//        }
+        String[][] Lunaa = {{"A", "U", "G"}, {"U", "S", "T"}};
+
+        for (int i = 0; i < Lunaa.length; i++) {
+            for (int j = 0; j < Lunaa[i].length; j++) {
+                if (Lunaa[i][j].equals("S")) {
+                    System.out.println("Am gasit litera");
+                    break;
+                }
+            }
+        }
+        System.out.println(" ");
+
 
         //continue - breaks an iteration of the loop
-//        int[] numbers = {1, 2, 3, 4, 5};
-//
-//        for (int i = 0; i < numbers.length; i++) {
-//            if (numbers[i] == 3) continue;
-//            System.out.println(numbers[i]);
-//        }
-//
+
+        int[] numbers = {5, 1, 8, 3, 6};
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0)
+                continue;
+            System.out.println(numbers[i]);
+
+        }
+        System.out.println(" ");
+
 
         //labels
+        int[][] numeree = {{2, 8, 5, 6}, {2, 7, 5, 4}, {4, 7, 5, 8}};
+        System.out.println(numeree.length);
+        System.out.println(numeree[0].length);
+        System.out.println(numeree[1].length);
+        System.out.println(numeree[2].length);
+        System.out.println(" ");
 
-//        int[][] numbers = {{2, 3, 4}, {5, 6, 7}, {8, 9, 0}};
-//        System.out.println(numbers.length);
-//        System.out.println(numbers[0].length);
-//        System.out.println(numbers[1].length);
 
         //break with label
-//        for (int i=0; i< numbers.length; i++){
-//            for (int j=0; j< numbers[i].length;j++){
-//                System.out.println(numbers[i][j]);
-//                break;
-//            }
-//        }
-//
-//        OUTER_LOOP: for (int i=0; i< numbers.length; i++){
-//            for (int j=0; j< numbers[i].length;j++){
-//                System.out.println(numbers[i][j]);
-//                break OUTER_LOOP;
-//            }
-//        }
+        for (int i = 0; i < numeree.length; i++) {
+            for (int j = 0; j < numeree[i].length; j++) {
+                System.out.println(numeree[i][j]);
+                break;
+            }
+        }
+        System.out.println(" ");
+
+
+        OUTER_LOOP:
+        for (int i = 0; i < numeree.length; i++) {
+            for (int j = 0; j < numeree[i].length; j++) {
+                System.out.println(numeree[i][j]);
+                break OUTER_LOOP;
+            }
+        }
+        System.out.println(" ");
+
 
         //continue with label
-//        for (int i = 0; i < numbers.length; i++) {
-//            for (int j = 0; j < numbers[i].length; j++) {
-//                if (numbers[i][j] % 3 == 0) {
-//                    continue;
-//                }
-//                System.out.println(numbers[i][j]);
-//            }
-//            System.out.println("iteration completed");
-//        }
 
-//        OUTER_LOOP: for (int i = 0; i < numbers.length; i++) {
-//            for (int j = 0; j < numbers[i].length; j++) {
-//                if (numbers[i][j] % 3 == 0) {
-//                    continue OUTER_LOOP;
-//                }
-//                System.out.println(numbers[i][j]);
-//            }
-//            System.out.println("iteration completed");
-//        }
-
-
-    }
-
-    private static void checkNumber(int x) {
-        if (x > 0) {
-            System.out.println("x is a positive number.");
+        for (int i = 0; i < numeree.length; i++) {
+            for (int j = 0; j < numeree[i].length; j++) {
+                if (numeree[i][j] % 3 == 0) {
+                    continue;
+                }
+                System.out.println(numeree[i][j]);
+            }
+            System.out.println("iteration completed");
         }
     }
-
 }
+
