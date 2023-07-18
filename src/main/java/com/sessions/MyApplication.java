bf17ab708050b7fa489c6098bf331bd667f55eb9
BogdanTicu <bogdan.ticu2003@gmail.com>
2023-07-18 21:21:08 +0300

    Sesiunea 6

--- src/main/java/com/sessions/MyApplication.java
+++ src/main/java/com/sessions/MyApplication.java
@@ -8,14 +8,6 @@ public class MyApplication {
             System.out.println("Hello World");
             System.out.println("Hello World");
 
-<<<<<<< HEAD
-        System.out.println("Hello world");
-        System.out.println("some updates");
-        System.out.println("some more updates");
-        System.out.println("Bogdan a fost aici");
-        
-=======
->>>>>>> main
     }
 
 }
