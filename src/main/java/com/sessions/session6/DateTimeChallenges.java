2b74f647fd4419b3d7fd081df1ecf6088df2ce26
catalinm-martazan <catalin.martazan@gmail.com>
2023-07-18 21:41:46 +0300

    Added session6 challenge class

--- src/main/java/com/sessions/session6/DateTimeChallenges.java
+++ src/main/java/com/sessions/session6/DateTimeChallenges.java
@@ -1,5 +1,31 @@
+/*
+
+[ ] 1.  Write a method that prints a LocalDate with the following format 12-2005-20 (month-year-day)
+
+[ ] 2.  Write a method that prints local time with the following format HH:mm:ss
+
+[ ] 3.  Write a method that calculates your age (you can use Period object)
+
+ */
+
 package com.sessions.session6;
 
 public class DateTimeChallenges {
-    //
+    public static void main(String[] args) {
+        /*-------------------
+         |  Exercise no. 1  |
+         -------------------*/
+
+
+        /*-------------------
+         |  Exercise no. 2  |
+         -------------------*/
+
+        /*-------------------
+         |  Exercise no. 3  |
+         -------------------*/
+
+    }
+
+
 }
