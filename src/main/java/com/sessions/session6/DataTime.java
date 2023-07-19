e98b4ffd83fcce8a0b1f4b8a4fd92f92134e9612
David Vasile <vasiled027@gmail.com>
2023-07-19 13:57:43 +0300

    ...

---
+++ src/main/java/com/sessions/session6/DataTime.java
@@ -0,0 +1,45 @@
+package com.sessions.session6;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class DataTime {
+
+    public static void main(String[] args){
+
+//         LocalDate myLocalDate = LocalDate.now();
+//         System.out.println(myLocalDate);
+//
+//
+//         System.out.println(myLocalDate.plusDays(2));
+////         myLocalDate.plus(2, ChronoUnit.DAYS);
+//
+//        LocalDate myLocalDate2=LocalDate.of(1993,12,10);
+//        System.out.println(myLocalDate2);
+//
+////        LocalDate myLocalDate3=LocalDate.of(1993,30,10);
+////        System.out.println(myLocalDate3);
+//
+//        boolean date1isBefore= myLocalDate.isBefore(myLocalDate2);
+//        System.out.println(date1isBefore);
+//
+//        ZoneId zoneId = ZoneId.of("Australia/Darwin");
+//        LocalDate localDateWithZone= LocalDate.now(zoneId);
+//        System.out.println(localDateWithZone);
+
+//        LocalDate myLocalDate2 = LocalDate.of(1993,10,12);
+//        System.out.println(myLocalDate2);
+//        System.out.println(myLocalDate2.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+//        System.out.println(myLocalDate2.format(DateTimeFormatter.ofPattern("yyyy-dd-MM")));
+//        System.out.println(myLocalDate2.format(DateTimeFormatter.ofPattern("yyyy-dd-MMMM")));
+
+        //parse
+//        String  myDateAsString="1990-10-11";
+//        LocalDate myLocalDateFromString=LocalDate.parse(myDateAsString,
+//                DateTimeFormatter.ofPattern("yyyy.MM.DD"));
+//        System.out.println(myLocalDateFromString.format(DateTimeFormatter.ofPattern("yyyy-dd-MM")));
+
+    }
+
+}
