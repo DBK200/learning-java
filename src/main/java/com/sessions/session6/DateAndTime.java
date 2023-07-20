ce25a4ae1fcc87caef5e4c9e91a1cd78624d328a
teo332 <bulzanteodor330@yahoo.com>
2023-07-20 23:46:22 +0300

    RandomAndScannerSession

--- src/main/java/com/sessions/session6/DateAndTime.java
+++ src/main/java/com/sessions/session6/DateAndTime.java
@@ -3,6 +3,9 @@ package com.sessions.session6;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Random;
+import java.util.Scanner;
 
 public class DateAndTime {
     public static void main(String[] args) {
@@ -34,12 +37,101 @@ public class DateAndTime {
 //        System.out.println(localDate.format(DateTimeFormatter.ofPattern("MMMM-dd-yyyy")));
 //        System.out.println(localDate.getDayOfMonth());
 
-        String myDateAsString = "1992-11-28";
-        LocalDate myLocalDateFromString = LocalDate.parse(myDateAsString);
-        System.out.println(myLocalDateFromString.isLeapYear());
-        System.out.println(myLocalDateFromString);
+//        String myDateAsString = "1992-11-28";
+//        LocalDate myLocalDateFromString = LocalDate.parse(myDateAsString);
+//        System.out.println(myLocalDateFromString.isLeapYear());
+//        System.out.println(myLocalDateFromString);
 
+//        Period period = Period.of(2,3,4);
+//        System.out.println(period);
+//        LocalDate localDate = LocalDate.of(1995,10,3);
+//        localDate = localDate.plus(period);
+//        System.out.println(localDate);
 
+//        LocalTime presentLocalTime = LocalTime.now();
+//        System.out.println(presentLocalTime);
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh:mm:ss");
+//        String timeformatted = presentLocalTime.format(formatter);
+//        System.out.println(timeformatted);
+//
+//
+//        Duration duration =Duration.ofHours(2);
+//        LocalTime localTimePlusPeriod = presentLocalTime.plus(duration);
+//        System.out.println(localTimePlusPeriod);
+//        Duration durationBetween = Duration.between(presentLocalTime, localTimePlusPeriod);
+//        System.out.println(durationBetween);
+
+//        String timeAsString = "10:20:19";
+//        LocalTime parsedLocalTime = LocalTime.parse(timeAsString);
+//        parsedLocalTime.compareTo(LocalTime.now());
+//        System.out.println(parsedLocalTime);
+//        System.out.println(parsedLocalTime.compareTo(LocalTime.now()));
+//        System.out.println(parsedLocalTime.equals(LocalTime.now()));
+
+//        LocalDateTime localDateTime = LocalDateTime.now();
+//        System.out.println(localDateTime);
+//        LocalDate localDate = LocalDate.of(1992, 12,11);
+//        LocalTime localTime = LocalTime.of(0,2,9, 02121021);
+//        LocalDateTime localDateTime1 = LocalDateTime.of(localDate, localTime);
+//        System.out.println(localDateTime1);
+
+//        LocalDateTime localDateTime = LocalDateTime.of(1993, 12, 12, 8, 14, 29,2090);
+//        System.out.println(localDateTime);
+
+//        LocalDate localDate = LocalDate.of(2004,12,2);
+//        System.out.println(localDate.isLeapYear());
+
+//        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+//        System.out.println(zonedDateTime);
+//        ZoneId zoneId = ZoneId.of("Asia/Tokyo");
+//        System.out.println(ZonedDateTime.now(zoneId));
+
+        //Date(java.util)
+//        Date date = new Date();
+//        date.setTime(11515115);
+//        System.out.println(date);
+//
+//        //Date(java.sql)
+//        java.sql.Date dateFromSql = new java.sql.Date(12345);
+//        System.out.println(dateFromSql);
+
+//        Random random = new Random();
+//        System.out.println(random.nextInt(5,10));
+//        System.out.println(random.nextBoolean());
+//        System.out.println(random.nextDouble(3,10));
+
+//            Scanner scanner = new Scanner(System.in);
+////        String myString = scanner.nextLine();
+////
+////        System.out.println("you inserted these words " + myString);
+//
+//        Random random = new Random();
+//        int userNumber = scanner.nextInt();
+//        int pcNumber =random.nextInt(10);
+//        System.out.println("You have inserted this number " + userNumber);
+//        System.out.println("Pc has generated this number " + pcNumber);
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int sum = 0;
+//        while (scanner.hasNext()){
+//            if(scanner.next().equalsIgnoreCase("x")){
+//                break;
+//            }
+//            sum += scanner.nextInt();
+//        }
+//        System.out.println("Sum is " + sum);
+//
+//        Scanner scanner = new Scanner(System.in);
+
+        Scanner scanner = new Scanner(System.in);
+
+//        int sum = 0;
+//        while (scanner.hasNextInt()){
+//
+//            sum += scanner.nextInt();
+//        }
+//        System.out.println("Sum is " + sum);
 
     }
 }
