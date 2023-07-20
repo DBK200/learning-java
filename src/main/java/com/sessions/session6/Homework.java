35b6e8cea59e475f25362942319c81d8b99c76ff
David Vasile <vasiled027@gmail.com>
2023-07-20 16:49:29 +0300

    session6 + Homework

---
+++ src/main/java/com/sessions/session6/Homework.java
@@ -0,0 +1,25 @@
+package com.sessions.session6;
+
+import java.time.*;
+public class Homework {
+
+    public static void main(String[] args) {
+//        System.out.println("1.");
+//        LocalDate myLocalDate = LocalDate.now();
+//        System.out.println("Data de astazi este "+myLocalDate);
+
+//        System.out.println("");
+//        System.out.println("2.");
+//        LocalDate myLocalDate = LocalDate.now();
+//        System.out.println("Suntem in anul "+myLocalDate.getYear()+" luna "+myLocalDate.getMonth()+" ziua " +myLocalDate.getDayOfMonth());
+
+//        System.out.println("");
+//        System.out.println("3.");
+//        LocalDate myLocalDate = LocalDate.of(2025,8,19);
+//        System.out.println(myLocalDate);
+
+    }
+}
+
+
+
