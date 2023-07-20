bf977ef23a9d9cde86a4dfe9497d2155df19573a
Marian <maryanionutz93@yahoo.com>
2023-07-20 17:06:47 +0300

    Date And Time Homework

--- src/main/java/com/sessions/session6/DateAndTime.java
+++ src/main/java/com/sessions/session6/DateAndTime.java
@@ -43,5 +43,7 @@ public class DateAndTime {
         System.out.println(myLocalDateFromString.getDayOfWeek());
         System.out.println(myLocalDateFromString.getYear());
 
+
+
     }
 }
---
+++ src/main/java/com/sessions/session6/DateAndTimeHomework.java
@@ -0,0 +1,43 @@
+package com.sessions.session6;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class DateAndTimeHomework {
+    public static void main(String[] args) {
+//        LocalDate myLocalDate = LocalDate.now();
+//        System.out.println(myLocalDate);
+//        System.out.println(myLocalDate.format(DateTimeFormatter.ofPattern("MM-yyyy-dd")));
+//        boolean a = myLocalDate.equals(LocalDate.now());
+//        System.out.println(a);
+//
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter a date");
+//        String date1 = input.next();
+//        System.out.println("Enter a date");
+//        String date2 = input.next();
+//        LocalDate date1FromString = LocalDate.parse(date1);
+//        LocalDate date2FromString = LocalDate.parse(date2);
+//        System.out.println(date1FromString);
+//        System.out.println(date2FromString);
+//        boolean datesAreEquals = date1FromString.equals(date2FromString);
+//        System.out.println(datesAreEquals);
+
+//        LocalDateTime myLocalDate1 = LocalDateTime.now();
+//        System.out.println(myLocalDate1);
+//        System.out.println(myLocalDate1.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+
+//        Scanner input1 = new Scanner(System.in);
+//        System.out.println("Enter birthday");
+//        String myBirthday = input1.next();
+//        LocalDate myBirthdayFromString = LocalDate.parse(myBirthday);
+//        LocalDate today = LocalDate.now();
+//        Period compare = Period.between(myBirthdayFromString, today);
+//        System.out.println(compare.getYears()+" years "+compare.getMonths()+ " months "+compare.getDays()+ " days ");
+
+
+    }
+}
