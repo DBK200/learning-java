9160fd559d3684b6c28d29e98c075ca9168db026
teo332 <bulzanteodor330@yahoo.com>
2023-07-27 15:51:29 +0300

    Homework session7

---
+++ src/main/java/com/sessions/session7/ArrayHomework.java
@@ -0,0 +1,68 @@
+package com.sessions.session7;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ArrayHomework {
+    public static void main(String[] args) {
+
+        //1. Create a shopping list with Array and print the values
+
+//        String[] shoppingList = {"Apples", "Milk", "Beers", "Chocolate", "Bread"};
+//        for (String i : shoppingList){
+//            System.out.println(i);
+//        }
+
+
+        //2. Create a wishlist for Christmas with ArrayList and print the values
+        //(I made the user to enter the elements)
+
+//        Scanner scanner = new Scanner(System.in);
+//        ArrayList<String> wishlist = new ArrayList<>();
+//        System.out.println("Enter what you wish for christmas: (enter 'q' to quit)");
+//
+//        while (true) {
+//            String input = scanner.nextLine();
+//            if (input.equalsIgnoreCase("q")) {
+//                break;
+//            }else {
+//                wishlist.add(input);
+//                System.out.println("Item added to wishlist");
+//            }
+//        }
+//        System.out.println("Your Christmas wishlist:");
+//        for (String item : wishlist){
+//            System.out.println("- " + item);
+//        }
+
+
+        //3. Create
+        //    - 2 empty ArrayLists: studentList and graduateStudentList
+        //    - populate studentList with 10 students
+        //    - copy values from studentList to graduateStudentList
+        //    - iterate through graduateStudentList and print each graduated student
+
+
+//        ArrayList<String> studentList = new ArrayList<>();
+//        ArrayList<String> graduateStudentList = new ArrayList<>();
+//
+//        studentList.add("Ion");
+//        studentList.add("Andrei");
+//        studentList.add("Alex");
+//        studentList.add("Laurentiu");
+//        studentList.add("Jan");
+//        studentList.add("Razvan");
+//        studentList.add("Ana");
+//        studentList.add("Daniela");
+//        studentList.add("Alberta");
+//        studentList.add("Ioana");
+//
+//        graduateStudentList.addAll(studentList);
+//
+//        System.out.println("Graduated Students:");
+//        for(String student : graduateStudentList) {
+//            System.out.println(student);
+//        }
+
+    }
+}
