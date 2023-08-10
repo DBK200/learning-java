1c5a16ca674c0a6234dc70d4f9b5760366feb6ce
Marian <maryanionutz93@yahoo.com>
2023-08-10 18:08:53 +0300

    Update Homework Session 11

--- src/main/java/com/sessions/session11/Homework/Exercise4/AreaM.java
+++ src/main/java/com/sessions/session11/Homework/Exercise4/AreaM.java
@@ -1,31 +1,32 @@
 package com.sessions.session11.Homework.Exercise4;
 
+import java.awt.geom.Area;
+
 public class AreaM extends ShapeM {
 
     public int rectangleArea(int length, int width) {
         //Area of a rectangle = length × width
-        length = 35;
-        width = 25;
         return length * width;
     }
 
     public int squareArea(int x) {
         //A=a2
-        x = 5;
         return x * 5;
     }
 
     public double circleArea(double area) {
         //Area = pi * r^2
-        double radius = 8 / 2; //raza cercului se imparte la 2 si este jumatate de diametru, daca diametrul este 8, atunci raza este 4
-        area = Math.PI * radius * radius;
         return area;
 
 
     }
 
     public static void main(String[] args) {
-
+        AreaM areaM = new AreaM();
+        areaM.rectangleArea(35,25);
+        areaM.squareArea(5);
+        double radius = 8 / 2; // raza cercului se imparte la 2 si este jumatate de diametru, daca diametrul este 8, atunci raza este 4
+        areaM.circleArea(Math.PI * (radius * radius));
     }
 
 }
