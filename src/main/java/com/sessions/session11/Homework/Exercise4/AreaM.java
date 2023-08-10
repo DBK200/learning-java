6219b8fb77360d31d1028e5f4ac508f0c929a672
Marian <maryanionutz93@yahoo.com>
2023-08-10 17:28:02 +0300

    Update Homework 11

--- src/main/java/com/sessions/session11/Homework/Exercise4/AreaM.java
+++ src/main/java/com/sessions/session11/Homework/Exercise4/AreaM.java
@@ -1,24 +1,30 @@
 package com.sessions.session11.Homework.Exercise4;
 
-public class AreaM extends ShapeM{
+public class AreaM extends ShapeM {
 
-    public int rectangleArea(int length, int width){
+    public int rectangleArea(int length, int width) {
+        //Area of a rectangle = length × width
         length = 35;
         width = 25;
         return length * width;
     }
-    public int squareArea(int x){
-        return 5 * 5;
+
+    public int squareArea(int x) {
+        //A=a2
+        x = 5;
+        return x * 5;
     }
-    public double circleArea(double area){
+
+    public double circleArea(double area) {
         //Area = pi * r^2
-         double radius = 8 / 2;
-         area = Math.PI * radius * radius;
-         return area;
+        double radius = 8 / 2;
+        area = Math.PI * (radius * radius);
+        return area;
 
 
     }
-    public static void main (String[]args){
+
+    public static void main(String[] args) {
 
     }
 
