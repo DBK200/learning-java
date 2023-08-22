0d5182024d6741d98efbc378a576be233f057142
catalin-martazan <catalin.martazan@gmail.com>
2023-08-22 23:12:46 +0300

    Modified setQuantity() method in Resources class

--- src/main/java/com/sessions/session12/challenges/exercise1/Resources.java
+++ src/main/java/com/sessions/session12/challenges/exercise1/Resources.java
@@ -7,14 +7,15 @@ import java.util.LinkedList;
  * This class is used to implement resources data.
  * @author  <b><i>Catalin Martazan</i></b>
  * */
-public class Resource {
+public class Resources {
     private String code;
     private String name;
     private String description;
-    private int quantity;
+    private int availableQuantity;
+    private int neededQuantity;
 
-    // Holds all [Resource] class created objects
-    private static LinkedList<Resource> resources = new LinkedList<>();
+    // Holds all [Resources] class created objects
+    private static LinkedList<Resources> resources = new LinkedList<>();
 
     // Unique ID value
     private long resourceId;
@@ -24,7 +25,7 @@ public class Resource {
     // and the allocated to [resId]
     private static long resourceNo;
 
-    private Resource(Object publisher, String code, String name, String description, int quantity) {
+    private Resources(Object publisher, String code, String name, String description, int quantity) {
         if (publisher instanceof Persons
                 && ((Persons) publisher).getEmployeeType().equals(EmployeeType.ADMINISTRATOR)
                 && !code.isBlank()
@@ -32,14 +33,14 @@ public class Resource {
             this.name = name;
             this.code = code;
             this.description = description;
-            if (quantity > 0) this.quantity = quantity;
+            if (quantity > 0) this.availableQuantity = quantity;
             resourceId = ++resourceNo;
             resources.add(this);
         }
     }
 
     /**
-     * Creates a new {@link Resource} class object after some validation.<br>
+     * Creates a new {@link Resources} class object after some validation.<br>
      * Replaces the usual constructor introducing duplicate checking.
      *
      * @param publisher   the person who does the operation.
@@ -50,13 +51,13 @@ public class Resource {
      * @param quantity    resource quantity.
      * @param msg          method's return message. This parameter is used to get a return message
      *                     after the method is executed. If no return message is wanted, {@code null} must be used.
-     * @return a new {@link Resource} class object,<br><i>or</i><br>
-     * {@code null} if there's an existing {@link Resource} class object that has the same
-     * {@code code} value in the {@link Resource#code} field, {@code publisher} doesn't have
+     * @return a new {@link Resources} class object,<br><i>or</i><br>
+     * {@code null} if there's an existing {@link Resources} class object that has the same
+     * {@code code} value in the {@link Resources#code} field, {@code publisher} doesn't have
      * {@link EmployeeType#ADMINISTRATOR}, or one or more input parameters are wrong.
      */
-    public static Resource createResource(Object publisher, String code, String name,
-                                          String description, int quantity, StringBuilder msg){
+    public static Resources createResource(Object publisher, String code, String name,
+                                           String description, int quantity, StringBuilder msg){
         // Initial validation
         if (!(publisher instanceof Persons)
                 || !((Persons) publisher).getEmployeeType().equals(EmployeeType.ADMINISTRATOR)
@@ -68,17 +69,17 @@ public class Resource {
         else {
             // Holds the finding result into a variable
             // for multiple usage
-            Resource findResult = findResource(code);
+            Resources findResult = findResource(code);
 
             if (findResult == null) {
                 // The direct call of the constructor can return, when its conditions are not met,
                 // a non-null object filled with default values for allocated variable types,
                 // thus was discarded and replaced with the next one
-                Resource resource = new Resource(publisher, code, name,
+                Resources resources = new Resources(publisher, code, name,
                         ((description == null || description.isBlank()) ? "(none)" : description), quantity);
-                resource = (resource.getCode() == null) ? null : resource;
-                Persons.setMessage((resource != null) ? resource.toString() : null, msg);
-                return resource;
+                resources = (resources.getCode() == null) ? null : resources;
+                Persons.setMessage((resources != null) ? resources.toString() : null, msg);
+                return resources;
             }
             else {
                 Persons.setMessage(String.format("[Err] A resource with [code: %s] already exists!", code), msg);
@@ -88,7 +89,7 @@ public class Resource {
     }
 
     /**
-     * Creates a new {@link Resource} class object after some validation.<br>
+     * Creates a new {@link Resources} class object after some validation.<br>
      * Replaces the usual constructor introducing duplicate checking.
      *
      * @param publisher   the person who does the operation.
@@ -98,13 +99,13 @@ public class Resource {
      * @param quantity    resource quantity.
      * @param msg          method's return message. This parameter is used to get a return message
      *                     after the method is executed. If no return message is wanted, {@code null} must be used.
-     * @return a new {@link Resource} class object,<br><i>or</i><br>
-     * {@code null} if there's an existing {@link Resource} class object that has the same
-     * {@code code} value in the {@link Resource#code} field, {@code publisher} doesn't have
+     * @return a new {@link Resources} class object,<br><i>or</i><br>
+     * {@code null} if there's an existing {@link Resources} class object that has the same
+     * {@code code} value in the {@link Resources#code} field, {@code publisher} doesn't have
      * {@link EmployeeType#ADMINISTRATOR}, or one or more input parameters are wrong.
      */
-    public static Resource createResource(Object publisher, String code, String name,
-                                          int quantity, StringBuilder msg){
+    public static Resources createResource(Object publisher, String code, String name,
+                                           int quantity, StringBuilder msg){
         return createResource(publisher, code, name, "", quantity, msg);
     }
 
@@ -120,8 +121,12 @@ public class Resource {
         return description;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public int getAvailableQuantity() {
+        return availableQuantity;
+    }
+
+    public int getNeededQuantity() {
+        return neededQuantity;
     }
 
     public long getResourceId() {
@@ -129,12 +134,16 @@ public class Resource {
     }
 
     /**
-     * This method sets the resource quantity value, depending on {@code publisher} rights.<br>
+     * This method adds or removes {@code quantity} from {@link Resources#availableQuantity},
+     * depending on {@code publisher} rights.<br>
+     * If {@code quantity} is a positive number, the value will be added to the value of
+     * {@link Resources#availableQuantity}, otherwise it will be removed from it.<br>
      * Rights depend on the value of {@link Persons#employeeType} field of the parameter {@code publisher}.
      *
-     * @param publisher  the person who does the operation. Must be {@link EmployeeType#ADMINISTRATOR}
-     *                   to be able to set a value.
-     * @param quantity    resource quantity.
+     * @param publisher the person who does the operation. Must be {@link EmployeeType#ADMINISTRATOR}
+     *                  to be able to set a value.
+     * @param quantity  quantity added or removed from {@link Resources#availableQuantity}.
+     *                  Positive for addition and negative for removal.
      * @param msg       method's return message. This parameter is used to get a return message
      *                  after the method is executed. If no return message is wanted,
      *                  {@code null} must be used.
@@ -144,8 +153,25 @@ public class Resource {
     public boolean setQuantity(Object publisher, int quantity, StringBuilder msg) {
         if (!(publisher instanceof Persons)
                 && ((Persons) publisher).getEmployeeType().equals(EmployeeType.ADMINISTRATOR)
-                && this.quantity != quantity) {
-            this.quantity = quantity;
+                && quantity != 0) {
+
+            // [neededQuantity] > 0 means [availableQuantity] = 0
+            if (this.neededQuantity > 0) {
+                this.neededQuantity -= quantity;
+                if (this.neededQuantity < 0) {
+                    this.availableQuantity = -this.neededQuantity;
+                    this.neededQuantity = 0;
+                }
+            }
+
+            // [availableQuantity] > 0 means [neededQuantity] = 0
+            else {
+                this.availableQuantity += quantity;
+                if (this.availableQuantity < 0) {
+                    this.neededQuantity = -this.availableQuantity;
+                    this.availableQuantity = 0;
+                }
+            }
             return true;
         }
         else {
@@ -155,22 +181,22 @@ public class Resource {
     }
 
     /**
-     * This method looks in the {@link Resource#resources} list for an entry
-     * that has {@code resourceId} in the {@link Resource#resourceId} field
+     * This method looks in the {@link Resources#resources} list for an entry
+     * that has {@code resourceId} in the {@link Resources#resourceId} field
      * (i.e., unique value field).
      *
      * @param resourceId resource ID to be searched
-     * @return a {@link Resource} object with given parameter,<br><i>or</i><br>
+     * @return a {@link Resources} object with given parameter,<br><i>or</i><br>
      * {@code null} if nothing's found.
      */
-    static public Resource findResource(long resourceId) {
+    static public Resources findResource(long resourceId) {
         // Initial validation
         if (resourceId <= 0) return null;
 
         // Navigates through the list and checks if there's
         // a resource with [resourceId]
-        for (Resource resource : resources) {
-            if (resource.getResourceId() == resourceId) return resource;
+        for (Resources resources : Resources.resources) {
+            if (resources.getResourceId() == resourceId) return resources;
         }
 
         // Final output
@@ -178,22 +204,22 @@ public class Resource {
     }
 
     /**
-     * This method looks in the {@link Resource#resources} list for an entry
-     * that has {@code code} in the {@link Resource#code} field
+     * This method looks in the {@link Resources#resources} list for an entry
+     * that has {@code code} in the {@link Resources#code} field
      * (i.e., unique value field).
      *
      * @param code the code to be searched
-     * @return a {@link Resource} object with given parameter,<br><i>or</i><br>
+     * @return a {@link Resources} object with given parameter,<br><i>or</i><br>
      * {@code null} if nothing's found.
      */
-    static public Resource findResource(String code) {
+    static public Resources findResource(String code) {
         // Initial validation
         if (code.isBlank()) return null;
 
         // Navigates through the list and gets the object
         // that has the [code] value in the [code] field
-        for (Resource resource : resources) {
-            if (resource.getCode().equalsIgnoreCase(code)) return resource;
+        for (Resources resources : Resources.resources) {
+            if (resources.getCode().equalsIgnoreCase(code)) return resources;
         }
 
         // Final output if nothing's found
@@ -201,39 +227,39 @@ public class Resource {
     }
 
     /**
-     * This method looks in the {@link Resource#resources} list for entries
-     * that have {@code lookup} in {@link Resource#name} or {@link Resource#description},
+     * This method looks in the {@link Resources#resources} list for entries
+     * that have {@code lookup} in {@link Resources#name} or {@link Resources#description},
      * depending on the value specified by the {@code field} parameter.
      *
      * @param lookup the term to be searched in the field specified by {@code field} parameter
      * @param field   initial letter of the field where to look:<br>
-     *                <b>D</b> for {@link Resource#description}<br>
-     *                Anything else for  {@link Resource#name}.
+     *                <b>D</b> for {@link Resources#description}<br>
+     *                Anything else for  {@link Resources#name}.
      * @param isEqual comparison flag. If true, {@code equalsIgnoreCase()} method will be used
      *                for comparison, otherwise {@code contains()}.
-     * @return an ArrayList of {@link Resource} objects that satisfy given parameters,<br><i>or</i><br>
+     * @return an ArrayList of {@link Resources} objects that satisfy given parameters,<br><i>or</i><br>
      * {@code null} if nothing's found.
      */
-    static public ArrayList<Resource> findResource(String lookup, String field, boolean isEqual) {
+    static public ArrayList<Resources> findResource(String lookup, String field, boolean isEqual) {
         // Initial validation
         if (lookup.isBlank()) return null;
 
         // Result holding variable
-        ArrayList<Resource> arResult = new ArrayList<>();
+        ArrayList<Resources> arResult = new ArrayList<>();
 
         // Navigates through the list and collects all resources
         // that have [lookup] in [name] or [description] fields,
         // depending on the value passed as [field] parameter
-        for (Resource resource : resources) {
+        for (Resources resources : Resources.resources) {
 
             // Comparison field value depending on the value
             // passed as [field] parameter.
             String sField = (field.toUpperCase().charAt(0) == 'D')
-                    ? resource.getDescription()
-                    : resource.getName();
+                    ? resources.getDescription()
+                    : resources.getName();
 
             boolean bFlag = (isEqual) ? sField.equalsIgnoreCase(lookup) : sField.contains(lookup);
-            if (bFlag) arResult.add(resource);
+            if (bFlag) arResult.add(resources);
         }
 
         // Final output
@@ -241,8 +267,8 @@ public class Resource {
     }
 
     /**
-     * @return formatted resource data:  {@link Resource#code} {@link Resource#name}
-     * {@code [}{@link Resource#description}{@code ]} {@code (id: }{@link Resource#resourceId}{@code )}
+     * @return formatted resource data:  {@link Resources#code} {@link Resources#name}
+     * {@code [}{@link Resources#description}{@code ]} {@code (id: }{@link Resources#resourceId}{@code )}
      */
     @Override
     public String toString() {
@@ -258,10 +284,10 @@ class ResourceTest {
     public static void main(String[] args) {
 /*
         for (int i = 1; i <= 10; i++) {
-            System.out.println(Resource.createResource("code" + i,"name" + i, "description " + i));
+            System.out.println(Resources.createResource("code" + i,"name" + i, "description " + i));
         }
         for (int i = 5; i <= 15; i++) {
-            System.out.println(Resource.createResource("code" + i,"name" + i));
+            System.out.println(Resources.createResource("code" + i,"name" + i));
         }
 */
     }
