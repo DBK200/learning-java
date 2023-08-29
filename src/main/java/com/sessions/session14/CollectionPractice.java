37be9e1f1a3f90897bb6acd290255887731ed7c0
David Vasile <vasiled027@gmail.com>
2023-08-29 21:07:14 +0300

    session14

---
+++ src/main/java/com/sessions/session14/CollectionPractice.java
@@ -0,0 +1,50 @@
+package com.sessions.session14;
+
+import java.util.*;
+
+public class CollectionPractice {
+
+    public static void main(String[] args) {
+
+        //create a list with the grades of the student
+//        ArrayList<Integer> grades=new ArrayList<>();
+//
+//        grades.add(5);
+//        grades.add(10);
+//        grades.add(6);
+//
+//        System.out.println(grades);
+//        System.out.println(grades.get(1));
+//
+//        grades.remove((Integer) 10);
+//        System.out.println(grades);
+//
+//        for (int i=0;i<grades.size();i++)
+//        {
+//            System.out.println(grades.get(i));
+//        }
+//        for (Integer grade:grades){
+//            System.out.println(grade);
+//        }
+//
+
+//        LinkedList<String> names = new LinkedList<>();
+//        names.add("Alex");
+//        names.add("John");
+//        names.addFirst("Mike");
+//        System.out.println(names);
+//        names.offer("Teo");
+//        System.out.println(names);
+//        names.offerFirst("Daniel");
+//        System.out.println(names);
+
+//        Set<String> weekDays=new HashSet<>();//sorting is not guaranteed
+//        Set<String> weekDays=new TreeSet<>();//sorting is done in "natural" order(or by implementing Comparable)
+//        Set<String> weekDays=new LinkedHashSet<>();//sorting is according to the sequence of adding the elements
+//        weekDays.add("Tuesday");
+//        weekDays.add("Monday");
+//        weekDays.add("Monday");
+//        System.out.println(weekDays);
+
+    }
+}
