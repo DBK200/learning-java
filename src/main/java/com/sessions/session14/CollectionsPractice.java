fba189afed9a1c0aa36377e6f564e0b55abbe0f4
andreea.hincu <andreeahincu57@gmail.com>
2023-08-29 23:34:41 +0300

    Practice session 14 Collections

---
+++ src/main/java/com/sessions/session14/CollectionsPractice.java
@@ -0,0 +1,67 @@
+package com.sessions.session14;
+
+import java.util.*;
+
+public class CollectionsPractice {
+
+    public static void main(String[] args) {
+
+        // Create a list with the grades of a student
+//        ArrayList<Integer> grades = new ArrayList<>();
+//        grades.add(5);
+//        grades.add(10);
+//        grades.add(6);
+//
+//        grades.remove((Integer) 10); // se face Casting ca sa se elimine elementul
+//        System.out.println(grades);
+//        System.out.println(grades.get(1));
+//
+//        for(int i = 0; i < grades.size(); i++){
+//            grades.get(i);
+//        }
+//
+//        for(Integer grade : grades){
+//            System.out.println(grade);
+//        }
+
+//        Deque<String> names = new LinkedList<>();
+////        names.add("Alex");
+////        names.add("John");
+//        names.addFirst("Mike");
+//        names.offerFirst(" ");
+//        names.offer("");
+        //System.out.println(names);
+
+      //  Set<String> weekDays = new HashSet<>();  // nu se permit duplicate
+        // sorting is not guaranteed
+        //Set<String> weekDays = new TreeSet<>();
+        // sorting is done in "natural" order (or by implementing Comparable)
+
+//        Set<String> weekDays = new LinkedHashSet<>();
+        // sorting is according to the sequence of adding elements (adica sortarea se face in functie de cum au fost adaugate elemenetele)
+        // setul se foloseste cand avem nevoie de date unice
+
+        // in liste sunt permise duplicate
+
+//        weekDays.add("Monday");
+//        weekDays.add("Monday");
+//        weekDays.add("Tuesday");
+//        System.out.println(weekDays);
+
+        //HashMap<String, String> emailAddressesMap = new HashMap();
+        TreeMap<String, String> emailAddressesMap = new TreeMap();
+        emailAddressesMap.put("George", "george@yahoo.com");
+        emailAddressesMap.put("Diana", "diana@yahoo.com");
+        System.out.println(emailAddressesMap);
+        System.out.println(emailAddressesMap.get("George"));
+
+//        for(String cheie : emailAddressesMap.keySet()) {
+//            System.out.println(emailAddressesMap.get(cheie));
+//        }
+
+        for(Map.Entry<String, String> entry : emailAddressesMap.entrySet()){
+            System.out.println(entry.getKey() + " has this address: " +
+                    entry.getValue());
+        }
+    }
+}
